f5126c45243a5f1204b1d293a1f4077344f33302
Jackson Martorano Sant'Ana <jackson.m.santana1@gmail.com>
2016-07-27 23:07:25 -0300

    Initial Configurations

---
+++ src/index.spec.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import ClassList from './index.js';
+
+describe('ClassList =>', () => {
+  it('CSSClassList() ', () => {});
+
+  it('contains() ', () => {});
+
+  it('add() ', () => {});
+
+  it('remove() ', () => {});
+
+  it('toggle() ', () => {});
+});
